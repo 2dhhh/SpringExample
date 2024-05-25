405b1a6527f6f100db23922a183692c58053ac15
2_donghh <rirws96@naver.com>
2024-05-25 22:35:28 +0900

    Part3 강의 부분 완료

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,37 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// @Configuration -> 어플리케이션의 구성 정보를 담당 하는 설정 정보
+@Configuration
+public class AppConfig {
+    // @Bean -> Spring 컨테이너에 등록
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(MemberRepository());
+    }
+
+    @Bean
+    public MemoryMemberRepository MemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(MemberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
+}
